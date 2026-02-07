905953dd148ad887d6a7a8776555f410d29caf0a
Adib Muhammad Maros <adibmaros12@gmail.com>
2026-02-07 10:38:37 +0700

    edit base userl

--- lib/swagger.ts
+++ lib/swagger.ts
@@ -14,7 +14,7 @@ const options = {
     },
     servers: [
       {
-        url: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000",
+        url: process.env.NEXT_PUBLIC_API_URL || "https://backend-api-one-mocha.vercel.app/",
         description: "API Server",
       },
     ],
